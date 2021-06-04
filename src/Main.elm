e5ea469174c6717809a2b17d571b66d8f98bb627
ithaca2020 <ithaca2020@sjtu.edu.cn>
2021-06-04 12:18:44 +0800

    tmp

--- src/Main.elm
+++ src/Main.elm
@@ -1,66 +1,245 @@
-module Main exposing (..)
+module Main exposing (main)
 
-import Playground exposing (..)
+import Browser
+import Browser.Events exposing (onAnimationFrameDelta, onKeyDown, onKeyUp)
+import Browser.Navigation exposing (Key)
+import Debug exposing (toString)
+import Html exposing (..)
+import Html.Attributes as HtmlAttr exposing (..)
+import Html.Events exposing (keyCode)
+import Json.Decode as Decode
+import Svg exposing (Svg)
+import Svg.Attributes as SvgAttr
+
+
+type alias Point =
+    { x : Float
+    , y : Float
+    }
+
+type LocationOne
+    = Left
+    |Right
+    |Middle
+type alias Location =
+    {
+  xlocation : LocationOne
+  ,ylocation: LocationOne
+    }
+
+type Keydir
+    = Key_right
+    | Key_left
+    | Key_none
+
+
+type Live_status
+    = Dead
+    | Alive
+
+
+type alias Brick =
+    { pos : Point
+    , half_length : Float
+    , half_width : Float
+    }
+
+
+type alias Paddle =
+    { pos : Point
+    , half_length : Float
+    , half_width : Float
+    }
+
+
+type alias Ball =
+    { pos : Point
+    , move_x : Float
+    , move_y : Float
+    , radius : Float
+    }
+
+
+type alias Model =
+    { brick : List Brick
+    , move_timer : Float
+    , paddle : Paddle
+    , ball : Ball
+    , live_statue : Live_status
+    }
+
+
+type Msg
+    = Key Keydir
+    | Tick Float
 
 
 main =
-    game view update { x = 200, y = 200 }
+    Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
 
 
-initLineLocation : Number -> List ( Number, Number )
-initLineLocation row =
+init : () -> ( Model, Cmd Msg )
+init a =
+    ( initModel, Cmd.none )
+
+
+window_length =
+    1000.0
+
+
+window_width =
+    1000.0
+
+
+brick_number =
+    ( 10, 8 )
+
+
+brick_half_width =
+    40
+
+
+brick_half_length =
+    50
+
+
+initModel : Model
+initModel =
+    Model (generatebricks brick_number) 0 (Paddle (Point 500.0 700.0) Key_none (generateRectLines_4 (Point 500.0 700.0))) (Ball (Point 100.0 400.0) 45.0 Alive)
+
+
+generatebricks : ( Int, Int ) -> List Brick
+generatebricks size =
     let
-        addPoNumber : Number -> List ( Number, Number ) -> List ( Number, Number )
-        addPoNumber elem lst =
-            ( elem, row ) :: lst
-    in
-    List.foldr addPoNumber [] [ -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 ]
+        rangex =
+            List.range 0 (Tuple.first size - 1)
 
+        rangey =
+            List.range 0 (Tuple.second size - 1)
 
-initLocation : Number -> List ( Number, Number )
-initLocation line =
-    let
-        addLine : Number -> List (List ( Number, Number )) -> List (List ( Number, Number ))
-        addLine elm2 lst2 =
-            initLineLocation elm2 :: lst2
+        line =
+            \y -> List.map (\x -> Tuple.pair x y) rangex
     in
-    List.foldr addLine [] [ 1, 2, 3, 4, 5 ]
+    List.map line rangey
         |> List.concat
+        |> List.map toFloatPoint
+        |> List.map generateonebrick
 
 
-initBricks : List ( Number, Number )
-initBricks =
+generateonebrick : ( Float, Float ) -> Brick
+generateonebrick position =
     let
-        tran : ( Number, Number ) -> ( Number, Number )
-        tran ( sx, sy ) =
-            ( 30 * sx, 10 * sy + 245 )
+        pos =
+            Point (2 * brick_half_length * Tuple.first position + 1) (2 * brick_half_width * Tuple.second position - 1)
     in
-    List.map tran (initLocation 3)
+    Brick pos 50 40
 
 
-brickSet : Color -> Shape
-brickSet brickColor =
-    let
-        moveBrick : ( Number, Number ) -> List Shape -> List Shape
-        moveBrick ( xd, yd ) lstBrick =
-            rectangle yellow 25 8
-                |> move xd yd
-                |> List.repeat 1
-                |> List.append lstBrick
-    in
-    List.foldr moveBrick [] initBricks
-        |> group
+toFloatPoint : ( Int, Int ) -> ( Float, Float )
+toFloatPoint ( x, y ) =
+    ( Basics.toFloat x, Basics.toFloat y )
 
 
-view computer shapes =
-    [ rectangle blue 50 20
-        |> move 0 -300
-        |> move shapes.x shapes.y
-    , brickSet yellow
-    ]
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+        |> updatepaddle msg
 
 
-update computer shapes =
-    { x = shapes.x + 3 * toX computer.keyboard
-    , y = shapes.y + 3 * toY computer.keyboard
+updatepaddle : Msg -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
+updatepaddle msg ( model, cmd ) =
+    case msg of
+        Tick elapsed ->
+            ( { model | move_timer = model.move_timer + elapsed }
+                |> ballBounce
+                |> ballMove
+                --|> check_brick
+            , cmd
+            )
+
+        Key keydir ->
+            ( model
+                |> ballBounce
+                |> ballMove
+                |> paddleMove keydir
+            , cmd
+            )
+
+
+ballBounce : Model -> Model
+ballBounce model =
+    ballWindow model
+        |> ballBrick
+
+
+ballWindow : Model -> Model
+ballWindow model =
+    if model.ball.pos.x <= model.ball.radius || model.ball.pos.x >= window_length - model.ball.radius then
+        { model | ball = { pos = model.ball.pos, move_x = -model.ball.move_x, move_y = model.ball.move_y, radius = model.ball.radius } }
+
+    else if model.ball.pos.y <= model.ball.radius || model.ball.pos.y >= window_width - model.ball.radius then
+        { model | ball = { pos = model.ball.pos, move_x = model.ball.move_x, move_y = -model.ball.move_y, radius = model.ball.radius } }
+
+    else
+        model
+
+ballBrick : Model->Model
+ballBrick model =
+
+
+ballMove : Model -> Model
+ballMove model =
+    { model | ball = { pos = helpBallMove model.ball.move_x model.ball.move_y model.ball.pos, move_x = model.ball.move_x, move_y = model.ball.move_y, radius = model.ball.radius } }
+
+getCloseClass: Model ->(Point,Location)
+getCloseClass model =
+
+
+paddleMove : Keydir -> Model -> Model
+paddleMove keydir model =
+    { model
+        | paddle =
+            { pos = helpPaddleMove keydir model.paddle.pos
+            , half_length = model.paddle.half_length
+            , half_width = model.paddle.half_width
+            }
     }
+
+
+helpBallMove : Float -> Float -> Point -> Point
+helpBallMove move_x move_y old_point =
+    Point (old_point.x + move_x) (old_point.y + move_y)
+
+
+helpPaddleMove : Keydir -> Point -> Point
+helpPaddleMove keydir point =
+    case keydir of
+        Key_left ->
+            Point (point.x - 5.0) point.y
+
+        Key_right ->
+            Point (point.x + 5.0) point.y
+
+        _ ->
+            point
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ onAnimationFrameDelta Tick
+        , onKeyDown (Decode.map key keyCode)
+        ]
+
+
+key : Int -> Msg
+key keycode =
+    case keycode of
+        37 ->
+            Key Key_left
+
+        39 ->
+            Key Key_right
+
+        _ ->
+            Key Key_none
